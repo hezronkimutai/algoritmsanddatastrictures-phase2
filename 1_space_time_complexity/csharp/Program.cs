b2ee57c100da92f9095a75a831a2af12f6c0b0ab
Hezzie <hezronchelimo.hc@gmail.com>
2024-12-23 10:05:02 +0300

    ignore java and csharp files

---
+++ 1_space_time_complexity/csharp/Program.cs
@@ -0,0 +1,13 @@
+﻿//  In the above code “Hello World” 
+// is printed only once on the screen. 
+
+// Hence the complexity is O(1).
+using System;
+using SpaceTimeComplexity;
+public class GFG{
+
+    static public void Main (){
+        SpaceTimeComplexity.TimeComplexity.Oone();
+    }
+}
+
---
+++ 1_space_time_complexity/csharp/TimeComplexity.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace SpaceTimeComplexity
+{
+
+    public class TimeComplexity
+    {
+
+        static public void Oone()
+        {
+            // “Hello World” is printed only once on the screen. 
+            // Hence the complexity is O(1).
+            Console.WriteLine("Hello World");
+        }
+    }
+}
